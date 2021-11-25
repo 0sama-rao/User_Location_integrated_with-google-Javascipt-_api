41e1c90287922476143f53bcf5c7dd20c6bf165e
0sama-rao <94981090+0sama-rao@users.noreply.github.com>
2021-11-25 22:24:31 +0500

    Add files via upload

---
+++ project1/src/places/pages/NewPlace.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const NewPlace =()=> {
+
+
+  return <form className="place-form">
+    <Input type="text" label="title"/>
+  </form>;
+
+
+
+};
+export default NewPlace;
